71a76f177560a363fe19ee4f255b4c5907c17c8d
Brian Toll <briantoll@fastmail.com>
2021-11-02 08:55:54 -0400

    cnew and create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,16 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
   def index
     @articles = Article.all
+  end
+
+  def new 
+    
+  end
+
+  def create
+    render plain: params[:article]
   end
 end
